503c26e38c766cb6cdd90ca78f9cece6d0a17d23
John Patrick <alvezwynsjohn@gmail.com>
2024-03-28 15:58:33 +0800

    Archive Module

--- resources/views/admin/utilities/archive/index.blade.php
+++ resources/views/admin/utilities/archive/index.blade.php
@@ -27,35 +27,177 @@
     
 
         <div class="row">
-          <div class="col-md-12 col-lg-12 mt-3">
-            <div class="card">
-                <div class="card-body">
 
-                  <div class="mt-4">
-                    <table class="table table-hover" id="sales-archive-table">
-                        <thead>
-                         <tr>
-                          <th>Invoice #</th>
-                          <th>Product Code</th>
-                          <th>Name</th>
-                          <th>Unit</th>
-                          <th>Price</th>
-                          <th>Qty</th>
-                          <th>Amount</th>
-                          <th>Payment method</th>
-                          <th>Order from</th>
-                          <th>Date time archived</th>
-                         </tr>
-                        </thead>
-                     </table>
-                  </div>
+<div class="col-md-12 col-lg-12 mt-2">
+    <div class="card">
+        <div class="card-body">
+
+            <ul class="nav nav-pills" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="salesreport-tab" data-toggle="tab" href="#salesreport" role="tab"
+                        aria-controls="salesreport" aria-selected="true">Sales Report</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pre-order-tab" data-toggle="tab" href="#pre-order" role="tab"
+                        aria-controls="pre-order" aria-selected="true">Inventory Report</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pending-tab" data-toggle="tab" href="#pending" role="tab"
+                        aria-controls="pending" aria-selected="false">Pending</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="prepared-tab" data-toggle="tab" href="#prepared" role="tab"
+                        aria-controls="prepared" aria-selected="false">Prepared</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="shipped-tab" data-toggle="tab" href="#shipped" role="tab"
+                        aria-controls="shipped" aria-selected="false">Pick-Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="completed-tab" data-toggle="tab" href="#completed" role="tab"
+                        aria-controls="completed" aria-selected="false">Completed</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" id="cancelled-tab" data-toggle="tab" href="#cancelled" role="tab" aria-controls="cancelled" aria-selected="false">Cancelled</a>
+                  </li> -->
+
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="salesreport" role="tabpanel"
+                    aria-labelledby="salesreport-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="sales-archive_table">
+                            <thead>
+                                <tr>
+                                <th>Invoice #</th>
+                                <th>Product Code</th>
+                                <th>Name</th>
+                                <th>Unit</th>
+                                <th>Price</th>
+                                <th>Qty</th>
+                                <th>Amount</th>
+                                <th>Payment method</th>
+                                <th>Order from</th>
+                                <th>Date time archived</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-        <!-- /.row (main row) -->
-        
-      </div><!-- /.container-fluid -->
+                <div class="tab-pane fade show" id="pre-order" role="tabpanel"
+                    aria-labelledby="pre-order-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="tbl-pre-order">
+                            <thead>
+                                <tr>
+                                    <th>Order #</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone number</th>
+                                    <th>Pre-Order Date</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="pending" role="tabpanel" aria-labelledby="pending-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="tbl-pending-order">
+                            <thead>
+                                <tr>
+                                    <th>Order #</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone number</th>
+                                    <th>Date Order</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="prepared" role="tabpanel" aria-labelledby="prepared-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="tbl-prepared-order">
+                            <thead>
+                                <tr>
+                                    <th>Order #</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone number</th>
+                                    <th>Date Order</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="shipped" role="tabpanel" aria-labelledby="shipped-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="tbl-shipped-order">
+                            <thead>
+                                <tr>
+                                    <th>Order #</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone number</th>
+                                    <th>Date Order</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="completed" role="tabpanel" aria-labelledby="completed-tab">
+                    <div class="mt-4">
+                        <table class="table table-hover" id="tbl-completed-order">
+                            <thead>
+                                <tr>
+                                    <th>Order #</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone number</th>
+                                    <th>Date Order</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <!-- <div class="tab-pane fade" id="cancelled" role="tabpanel" aria-labelledby="cancelled-tab">
+                    <div class="mt-4">
+                      <table class="table table-hover" id="tbl-cancelled-order">
+                        <thead>
+                            <tr>
+                                <th>Order #</th>
+                                <th>Student Name</th>
+                                <th>Email</th>
+                                <th>Phone number</th>
+                                <th>Date Order</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    </div>
+                  </div> -->
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+<!-- /.row (main row) -->
+
+</div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
 
