0f21432552555a9693277e8c4f8204bf0aa3df71
John Patrick <alvezwynsjohn@gmail.com>
2024-03-28 14:21:52 +0800

    Replacement Report With Archive test 15

--- resources/views/admin/reports/replacement.blade.php
+++ resources/views/admin/reports/replacement.blade.php
@@ -69,5 +69,26 @@
 </section>
 <!-- /.content -->
 
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Archive Request</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p class="delete-message"></p>
+          <small class="validation-text text-danger"></small>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-sm btn-outline-dark btn-confirm-archive" type="button">Yes</button>
+          <button class="btn btn-sm btn-danger" data-dismiss="modal">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 @endsection
