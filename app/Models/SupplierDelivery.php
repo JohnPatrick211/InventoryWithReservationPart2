c699375eed5681ea06ac14ea0d9a3b5406a03d14
Blankz <alvezwynsjohn@gmail.com>
2024-04-13 14:14:10 +0800

    Supplier Delivery part 17

--- app/Models/SupplierDelivery.php
+++ app/Models/SupplierDelivery.php
@@ -22,6 +22,54 @@ class SupplierDelivery extends Model
         'date_delivered'
     ];
 
+    public function readSupplierDeliveryReport($supplier_id, $date_from, $date_to){
+        if($supplier_id == 'All'){
+            return DB::table('supplier_delivery AS SD')
+            ->select('SD.*', 'P.*',
+                    'SD.remarks',
+                    'SD.id as id',
+                    'SD.updated_at as updated',
+                    'PO.qty_order',
+                    'U.name as unit', 
+                    'S.supplier_name as supplier', 
+                    'C.name as category',
+                    DB::raw('CONCAT(SD.prefix, SD.id) as del_no'),
+                    'SD.date_delivered')
+            ->leftJoin('product as P', DB::raw('CONCAT(P.prefix, P.id)'), '=', 'SD.product_code')
+            ->leftJoin('purchase_order AS PO', 'PO.id', '=', 'SD.po_id')
+            ->leftJoin('supplier as S', 'S.id', '=', 'P.supplier_id')
+            ->leftJoin('category as C', 'C.id', '=', 'P.category_id')
+            ->leftJoin('unit as U', 'U.id', '=', 'P.unit_id')
+            ->where('PO.status', 4)
+            ->where('SD.archive_status', '!=', 0)
+            ->whereBetween(DB::raw('DATE(SD.date_delivered)'), [$date_from, $date_to])
+            ->get();
+        }
+        else{
+            return DB::table('supplier_delivery AS SD')
+            ->select('SD.*', 'P.*',
+                    'SD.remarks',
+                    'SD.id as id',
+                    'SD.updated_at as updated',
+                    'PO.qty_order',
+                    'U.name as unit', 
+                    'S.supplier_name as supplier', 
+                    'C.name as category',
+                    DB::raw('CONCAT(SD.prefix, SD.id) as del_no'),
+                    'SD.date_delivered')
+            ->leftJoin('product as P', DB::raw('CONCAT(P.prefix, P.id)'), '=', 'SD.product_code')
+            ->leftJoin('purchase_order AS PO', 'PO.id', '=', 'SD.po_id')
+            ->leftJoin('supplier as S', 'S.id', '=', 'P.supplier_id')
+            ->leftJoin('category as C', 'C.id', '=', 'P.category_id')
+            ->leftJoin('unit as U', 'U.id', '=', 'P.unit_id')
+            ->where('P.supplier_id', $supplier_id)
+            ->where('PO.status', 4)
+            ->where('SD.archive_status', '!=', 0)
+            ->whereBetween(DB::raw('DATE(SD.date_delivered)'), [$date_from, $date_to])
+            ->get();
+        }
+    }
+
     public function readSupplierDelivery($supplier_id, $date_from, $date_to){
         if($supplier_id == 'All'){
             return DB::table('supplier_delivery AS SD')
