02e707c1927e69d7faddeba8d47b8e3efa2dfea8
Blankz <alvezwynsjohn@gmail.com>
2024-04-14 17:07:52 +0800

    Supplier Delivery part 24

--- resources/views/admin/sales/cashiering.blade.php
+++ resources/views/admin/sales/cashiering.blade.php
@@ -112,7 +112,7 @@
                                             <div class="input-group mb-3">
                                                 <div class="input-group-prepend">
                                                 </div>
-                                                <input  type="number" id="contactno" step=".01" class="form-control">
+                                                <input  type="number" id="contactno" step=".01" class="form-control" disabled>
                                             </div>
                                         </th>
                                     </tr>
