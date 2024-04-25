339162427e244a867f1cf9766175254d0f774f0c
Blankz <alvezwynsjohn@gmail.com>
2024-04-25 23:16:24 +0800

    Supplier In Order

--- resources/views/admin/utilities/user/index.blade.php
+++ resources/views/admin/utilities/user/index.blade.php
@@ -32,6 +32,13 @@
           {{ \Session::get('success') }}
         </div>
 
+        @if(\Session::has('error'))
+        <div class="alert alert-danger alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h5><i class="icon fas fa-check"></i> </h5>
+          {{ \Session::get('error') }}
+        </div>
+
        
         @endif
 
